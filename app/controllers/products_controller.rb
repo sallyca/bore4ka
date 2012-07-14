99eb4020f073283a5e3ad7c736db9f65a53945c3
Sallyca <catherine.vladimirskaya@gmail.com>
2012-07-15 00:08:34 +0400

    product categorization

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < InheritedResources::Base
+
+	def index
+    	@cat = nil
+    	if params[:category].present?
+      		@cat = Category.find(params[:category])
+      		@products = @cat.products
+    	else
+      		@products = Product.limit(10).order('created_at DESC')
+    	end
+	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+	belongs_to :category
 	has_one :image, :as => :entity, :dependent => :destroy
  	accepts_nested_attributes_for :image, :allow_destroy => true, :reject_if => proc {|obj| obj.blank? }
 end
